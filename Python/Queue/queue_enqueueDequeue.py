6621e2b19cc864707bff02b46dcb005fb16d2233
MahediBuilds <zainmahedi.cs23@bmsce.ac.in>
2026-08-17 21:18:41 +0530

    Add queue_enqueueDequeue.py and queue_processPeople.py for basic queue operations including enqueue and dequeue functionality

---
+++ Python/Queue/queue_enqueueDequeue.py
@@ -0,0 +1,11 @@
+queue = []
+
+queue.append(10)
+queue.append(20)
+queue.append(30)
+
+front = queue.pop(0)
+
+print(front)
+
+print(queue)
---
+++ Python/Queue/queue_processPeople.py
@@ -0,0 +1,5 @@
+queue = ["Ali", "Zain", "Ahmed", "Sara"]
+
+while len(queue):
+    front = queue.pop(0)
+    print("Serving : " + front)
