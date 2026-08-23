22560ca96cb0cef78ffa82c54f53c836db2182c8
MahediBuilds <zainmahedi.cs23@bmsce.ac.in>
2026-08-23 12:09:16 +0400

    Add function to find the duplicate number in an array using Floyd's Tortoise and Hare algorithm

---
+++ 287.py
@@ -0,0 +1,27 @@
+class Solution:
+    def findDuplicate(self, nums):
+        slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        fast = nums[0]
+
+        while slow != fast:
+            slow = nums[slow]
+            fast = nums[fast]
+
+        return slow
+
+
+nums = [1, 3, 4, 2, 3]
+
+solution = Solution()
+result = solution.findDuplicate(nums)
+
+print(result)
