dd1cd84c6cbd1e56c10ce7880f4db33457b24ed1
MahediBuilds <zainmahedi.cs23@bmsce.ac.in>
2026-09-02 13:13:15 +0530

    Revision

--- 18.py
+++ 18.py
@@ -7,25 +7,24 @@ class Solution:
         for i in range(n):
             if i > 0 and nums[i] == nums[i - 1]:
                 continue
-
             for j in range(i + 1, n):
-                if j > i + 1 and nums[j] == nums[j - 1]:
+                if j > 0 and nums[j] == nums[j - 1]:
                     continue
 
                 l = j + 1
                 r = n - 1
 
                 while l < r:
-                    fourS = nums[i] + nums[j] + nums[l] + nums[r]
+                    fsum = nums[i] + nums[j] + nums[l] + nums[r]
 
-                    if fourS < target:
+                    if fsum < target:
                         l += 1
-                    elif fourS > target:
+                    elif fsum > target:
                         r -= 1
                     else:
                         result.append([nums[i], nums[j], nums[l], nums[r]])
-
                         l += 1
+
                         while l < r and nums[l] == nums[l - 1]:
                             l += 1
 
