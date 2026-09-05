f6d209e46b33f906e9a8691d688974e17e68e445
MahediBuilds <zainmahedi.cs23@bmsce.ac.in>
2026-09-06 03:57:51 +0530

    Add implementation of ListNode and hasCycle function to detect cycles in a linked list

---
+++ 141.py
@@ -0,0 +1,36 @@
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+
+class Solution:
+    def hasCycle(self, head):
+
+        slow = head
+        fast = head
+
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+
+            if slow == fast:
+                return True
+
+        return False
+
+
+# Create linked list
+head = ListNode(1)
+head.next = ListNode(2)
+head.next.next = ListNode(3)
+head.next.next.next = ListNode(4)
+
+# Create cycle: 4 -> 2
+head.next.next.next.next = head.next
+
+
+solution = Solution()
+result = solution.hasCycle(head)
+
+print(result)
